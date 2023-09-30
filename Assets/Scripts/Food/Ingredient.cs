0335c2d2339d300d7129e2bd60a05ca4a8965ed6
bryant-ta <33585269+bryant-ta@users.noreply.github.com>
2023-09-30 01:32:32 -0700

    ingredient

--- Assets/Scripts/Food/Ingredient.cs
+++ Assets/Scripts/Food/Ingredient.cs
@@ -1,17 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ingredient : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public SO_Ingredient ingredient;
+    
+    public int cookTime;
+    public bool cooked;
+
+    public List<Vector2Int> shape;
+
+    private void Start()
+    {
+        cookTime = ingredient.cookTime;
+    }
+
+    public void Place()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PickUp()
     {
         
     }
---
+++ Assets/Scripts/Food/SO_Ingredient.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Food/SO_Ingredient")]
+public class SO_Ingredient : ScriptableObject
+{
+    public string name;
+    public FoodType FoodType;
+    public int cookTime;
+    public List<Vector2Int> shape;
+}
+
+public enum FoodType
+{
+    Meat = 0,
+}
